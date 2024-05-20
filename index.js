c0792583e9f2b2d4d5b5faf25264e174b226196c
Gustavo Menezes <103705903+Decompilationist@users.noreply.github.com>
2024-05-20 14:05:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ function getMacroVBA(username) {
     }
 }
 
+// Função para buscar todos os títulos das macros VBA do arquivo .env e formatá-los
+function getAllMacroTitles() {
+    const macroTitles = new Set();
+    for (const key in process.env) {
+        if (key.startsWith('MACRO_VBA')) {
+            const macroName = key.substring(key.indexOf("_") + 1);
+            const macroTitle = macroName.split('_').slice(1).join('_'); // Removendo "VBA" do título
+            const formattedTitle = macroTitle.replace(/\d+$/, '') // Remove números no final
+                                              .replace(/_/g, ' ') // Substitui underscores por espaços
+                                              .toLowerCase() // Transforma em minúsculas
+                                              .replace(/(?:^|\s)\S/g, char => char.toUpperCase()); // Capitaliza primeira letra de cada palavra
+            macroTitles.add(formattedTitle);
+        }
+    }
+    return [...macroTitles];
+}
+
+
 function authenticate(req, res, next) {
     const user = basicAuth(req);
     if (user && user.pass === PASS) {
@@ -47,12 +65,28 @@ function authenticate(req, res, next) {
     }
 }
 
+// Função para autenticar com base apenas na senha do arquivo .env
+function authenticateadmin(req, res, next) {
+    const credentials = basicAuth(req);
+    if (credentials && credentials.pass === PASS) {
+        return next();
+    } else {
+        res.set('WWW-Authenticate', 'Basic realm="example"');
+        return res.status(401).send('Autenticação necessária.');
+    }
+}
+
 app.use('/macro', authenticate);
 
 app.get('/macro', (req, res) => {
     // Retorna a macro VBA correspondente ao usuário
     res.type('text/plain');
     res.send(req.macroVBA);
+});
+
+app.get('/macros', authenticateadmin, (req, res) => {
+    // Retorna os títulos das macros VBA do arquivo .env
+    res.json(getAllMacroTitles());
 });
 
 app.get('/', (req, res) => {
